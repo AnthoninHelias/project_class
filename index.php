486b4a48fdd7857eaf52c13a06dff316e3162087
undragon322 <93834296+undragon322@users.noreply.github.com>
2022-02-09 14:26:29 +0100

    suite

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 include 'ISport.inc';
 include 'sport.php';
 include 'sportRelais.php';
+include 'sportBallon.inc';
 
 
 echo "projet heritage et interface <br>";
@@ -9,3 +10,5 @@ $listSport=new Sport("Football", 11);
 echo $listSport->getDescription();
 $listSportRelais = new sportRelais("4*100",1,400);
 echo $listSportRelais->getDescription();
+$listSportBallon= new sportBallon("Handball",8,40,50);
+echo $listSportBallon->getDescription();
