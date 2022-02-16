d4b728103162a1774f033c48f936e6a9e987f061
undragon322 <93834296+undragon322@users.noreply.github.com>
2022-02-16 10:38:23 +0100

    suite

--- data.php
+++ data.php
@@ -3,34 +3,35 @@
 // Init les clubs
 $club = array();
 $club[1] = new Club(1, "ST Etienne", 777);
-$club[2] = new Club(2, "PSG", 666);
+$club[2] = new Club(2, "Paris", 666);
 $club[3] = new Club(3, "Bordeaux", 555);
 
 // Les sports de ST Etienne
 $sb = new SportBallon("Basketball", 5, 35, 45);
 $sportRelais1 = new SportRelais("3 * 400", 1, 1500);
+$sportRelais4 = new SportRelais("500m", 1, 100);
 $club[1]->AjouterSport($sb);
 $club[1]->AjouterSport($sportRelais1);
+$club[1]->AjouterSport($sportRelais4);
 
-
-// Les sports de PSG
+// Les sports de Paris
 $sport1 = new Sport("Karate", 1);
 $sport2 = new Sport("boxe", 1);
 $sportBallon1 = new SportBallon("Football", 11, 68, 105);
 $sportBallon2 = new SportBallon("Handball", 7, 35, 65);
-$sportRelais1 = new SportRelais("3 * 500", 1, 1500);
+$sportRelais2 = new SportRelais("3 * 400", 1, 1500);
 $club[2]->AjouterSport($sport1);
 $club[2]->AjouterSport($sport2);
 $club[2]->AjouterSport($sportBallon1);
 $club[2]->AjouterSport($sportBallon2);
-$club[2]->AjouterSport($sportRelais1);
+$club[2]->AjouterSport($sportRelais2);
 
 // Les sports Bordeaux
 $sport3 = new Sport("Cyclisme", 1);
 $sportBallon3 = new SportBallon("Football", 11, 68, 105);
 $sportBallon4 = new SportBallon("Rugby", 15, 65, 115);
-$sportRelais2 = new SportRelais("100m", 1, 100);
+$sportRelais3 = new SportRelais("100m", 1, 100);
 $club[3]->AjouterSport($sport3);
 $club[3]->AjouterSport($sportBallon3);
 $club[3]->AjouterSport($sportBallon4);
-$club[3]->AjouterSport($sportRelais2);
+$club[3]->AjouterSport($sportRelais3);
